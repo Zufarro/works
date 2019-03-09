c4e4e704b2b8b24ffad5cb4e95266cde27f2c68d
Zufarro <zufar-galimzyanov@yandex.ru>
2019-03-09 13:58:51 +0300

    model main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+class Person():
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        task_list = []
+        complete_task_list = []
+
+    def add(self):
+        pass
+
+    def complete(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def show_complete(self):
+        pass
+
+    def show_uncompleted(self):
+        pass
+
+    def show_uncompleted_numbers(self):
+        pass
+
+class Task():
+    def __init__(self, task_name, duration, start_time):
+        self.task_name = task_name
+        self.duration = duration
+        self.start_time = start_time
+        status = new
