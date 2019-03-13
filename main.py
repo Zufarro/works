7043be2453f93e6a75edc8f8e75686c80d99f4ef
Zufarro <zufar-galimzyanov@yandex.ru>
2019-03-13 19:08:22 +0300

    bugs fixed

--- main.py
+++ main.py
@@ -2,68 +2,38 @@ class Person():
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-        self.all_task_list = {}
-        self.complete_task_list = {}
-        self.uncomplete_task_list = {}
-
-    @classmethod
-    def get_key(cls,d):
-        for k in d.keys():
-            return(k)
+        self.task_list = []
+        self.complete_tasks_list = []
 
     def add(self,task):
-        if self.get_key(task) not in self.all_task_list:
-            print ("added new task: " + str(self.get_key(task)))
+        if task not in self.task_list:
+            print ("added new task: " + task[0])
+            self.task_list.append(task)
         else:
-            print ("changed task:" + str(self.get_key(task)))
-        self.all_task_list.update(task)
-        self.uncomplete_task_list.update(task)
+            print ("the task is already on the task list")
 
     def complete(self,task):
-        comp_task = self.get_key(task)
-        if comp_task in self.uncomplete_task_list:
-            self.all_task_list.get(comp_task)[2] = "complete"
-            del self.uncomplete_task_list[comp_task]
-            self.complete_task_list.update(task)
-            print("task "+str(comp_task)+" complete")
+        if task in self.task_list:
+            self.task_list.remove(task)
+            task[3] = "complete"
+            self.complete_tasks_list.append(task)
+            print("task "+task[0]+" complete")
         else:
             print("error")
 
-    def clear(self, list):
-        if list == "all":
-            self.uncomplete_task_list.clear()
-            self.complete_task_list.clear()
-            self.all_task_list.clear()
-            print("all your liust of tasks cleared")
-        elif list == "uncomplete_task_list":
-            for i in self.uncomplete_task_list:
-                del self.all_task_list[i]
-            self.uncomplete_task_list.clear()
-            print("your uncomplete tasks cleared")
-        elif list == "complete_task_list":
-            self.uncomplete_task_list.clear()
-            print("your list of complete tasks cleared")
-        else:
-            print("error")
+    def clear(self):
+        self.task_list.clear()
+        print("task list cleared")
 
     def show_completes_numbers(self):
-        i = 0
-        for k in self.complete_task_list:
-            i += 1
-        print("Number of completed tasks: "+str(i))
+        print(self.complete_tasks_list)
 
     def show_uncompletes(self):
-        if self.uncomplete_task_list:
-            print("uncompletes task:")
-            for k in self.uncomplete_task_list:
-                print(k)
-        else:
-            print("uncompletes did not find")
+        print(self.tasks_list)
+
     def show_uncompletes_numbers(self):
-        i = 0
-        for k in self.uncomplete_task_list:
-            i += 1
-        print("Number of uncompleted tasks: "+str(i))
+        print(len(self.tasks_list))
+
 
 class Task():
     def __init__(self, task_name, duration, start_time):
@@ -71,8 +41,4 @@ class Task():
         self.duration = duration
         self.start_time = start_time
         self.status = "new"
-
-    @property
-    def choose(self):
-        d = {self.task_name : [self.duration, self.start_time, self.status]}
-        return d
+        self.all_info = [self.task_name, self.duration, self.start_time, self.status]
--- using.py
+++ using.py
@@ -6,19 +6,15 @@ task2 = Task("defeat the Joker", "1 month", "2008")
 task3 = Task("to retire", "1 year", "2012")
 
 # add all tasks:
-user1.add(task1.choose)
-user1.add(task2.choose)
-user1.add(task3.choose)
-print(user1.all_task_list)
+user1.add(task1.all_info)
+user1.add(task2.all_info)
+user1.add(task3.all_info)
+print(user1.task_list)
 
 #finish task1:
-user1.complete(task1.choose)
-print(user1.all_task_list)
-user1.show_uncompletes()
-user1.show_uncompletes_numbers()
+user1.complete(task1.all_info)
+print(user1.task_list)
 user1.show_completes_numbers()
 
-#cler uncomplete task
-user1.clear("uncomplete_task_list")
-user1.show_uncompletes()
-print(user1.all_task_list)
+
+user1.clear()
